dd578a47a6e3f3e93f1fdbab28e71e21b398c57e
LenaIT <olena.denysova@gmail.com>
2020-10-23 13:56:11 +0200

    routes

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const connectDB = require('./config/db')
+
+const app = express()
+
+//connect DB
+connectDB()
+
+//Init Middleware
+//allows to get dates on req.body
+app.use(express.json({extended: false}))
+
+app.get('/', (req, res) => {
+    res.send("API is running")
+})
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
